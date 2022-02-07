b5f4e0a672d96dd81d8c94f42a3276ef8baad1cb
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2022-02-07 16:34:20 +0100

    Changes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const AWS = require('aws-sdk');
+const config = require('./config');
 
 const app = express();
 
@@ -12,12 +14,22 @@ const Destination = require('./destination.js');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
+app.use('/img', express.static('img'));
+app.use('/styles', express.static('styles'));
+app.use('/scripts', express.static('scripts'));
+
+AWS.config.update(config.s3);
+const s3 = new AWS.S3();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+String.prototype.capitalize = function () {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+}
 
 app.get('/', async function (req, res, next) {
   let destinations = [];
@@ -59,5 +71,58 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.get('/', function (req, res) {
+
+  const params = {
+    Bucket: config.s3.bucket,
+    Delimiter: '/'
+  };
+
+  s3.listObjects(params, function (err, data) {
+    if (err) {
+      res.send(err);
+    } else {
+      const images = [];
+      console.log(data)
+      for (i in data.Contents) {
+        const image = data.Contents[i];
+        const imageProps = {
+          url: '/file/' + image.Key,
+          full_suffix: config.suffix.full,
+          rotation: (Math.random() * 20 - 10)
+        };
+        images.push(imageProps);
+      }
+      res.render('index', {
+        'name': 'Gallery',
+        'images': images
+      });
+    }
+  });
+});
+
+app.get('/file/:filename', function (req, res) {
+  const params = {
+    Bucket: config.s3.bucket,
+    Key: req.params.filename,
+  };
+
+  s3.getObject(params, function (err, data) {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log(data);
+      res.contentType(data.ContentType);
+      res.send(data.Body);
+    }
+  });
+})
+
+/*const server = app.listen(port, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('app listening at http://%s:%s', host, port);
+});*/
 
 module.exports = app;
---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'WKCU8QTK8JV8ISUAPLLG',
+    secretAccessKey: 'vvTAWielTGsPwj9EKerXzAk98Dw5KMsvGlker5YS',
+    endpoint: 's3.eu-west-1.wasabisys.com',
+    bucket: 'our-holidays',
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
