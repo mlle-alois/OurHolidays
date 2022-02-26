a54ee158208c411176ae5f3124e674a8b9469492
ame <amedouillard@hotmail.fr>
2022-02-26 12:56:19 +0100

    maj config wasabi

--- config.js
+++ config.js
@@ -1,12 +1,17 @@
 module.exports = {
+  //Stockage des images : Wasabi
   s3: {
-    accessKeyId: 'WKCU8QTK8JV8ISUAPLLG',
-    secretAccessKey: 'vvTAWielTGsPwj9EKerXzAk98Dw5KMsvGlker5YS',
+    accessKeyId: 'Z6THZ90I1DITCB39DL1Y',
+    secretAccessKey: 'wBPygdegDIH1gg3W4Vs0LwRRWW34iPDkjBVr2TSi',
     endpoint: 's3.eu-west-1.wasabisys.com',
-    bucket: 'our-holidays',
+    bucket: 'our-holidays-test',
   },
   suffix: {
     small: '_small.jpg',
     full: '_full.jpg'
-  }
+  },
+  // blitline: {
+  //   //heroku config:get BLITLINE_APPLICATION_ID
+  //   BLITLINE_APPLICATION_ID: '4AZkVNR_s-SVOXOT_jRL8mw'
+  // }
 };
