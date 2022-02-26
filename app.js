583a194748a61efb5d3e451bfc8391129dd87bbf
ame <amedouillard@hotmail.fr>
2022-02-26 12:59:26 +0100

    Déplacement des routes dans fichier routes + upload images + validation formulaire simple

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const request = require("request")
-const createError = require('http-errors');
-const express = require('express');
 const path = require('path');
+const express = require('express');
+
+const routes = require('./routes');
+
+const request = require("request");
+const createError = require('http-errors');
+
+const nodeUtil = require('util');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const AWS = require('aws-sdk');
-const config = require('./config');
 
 const app = express();
-
-// database dependency
-const Destination = require('./destination.js');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,118 +18,17 @@ app.set('view engine', 'twig');
 app.use('/img', express.static('img'));
 app.use('/styles', express.static('styles'));
 app.use('/scripts', express.static('scripts'));
-
-AWS.config.update(config.s3);
-const s3 = new AWS.S3();
-
 app.use(logger('dev'));
+app.use('/', routes);
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const recipients = [
-    //{"email": "alois.zimmermann45@gmail.com"},
-    {"email": "alois.rosenthal45@gmail.com"},
-    //{"email": "ichtitski@gmail.com"},
-    //{"email": "amedouillard@hotmail.fr"}
-]
 
 String.prototype.capitalize = function () {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
-
-app.get('/', async function (req, res, next) {
-    let destinations = [];
-    try {
-        destinations = await Destination.findAll();
-    } catch (e) {
-    }
-    const params = {
-        Bucket: config.s3.bucket,
-        Delimiter: '/'
-    };
-
-    const images = [];
-    s3.listObjects(params, function (err, data) {
-        if (err) {
-            res.send(err);
-        } else {
-            console.log(data)
-            for (i in data.Contents) {
-                const image = data.Contents[i];
-                const imageProps = {
-                    url: '/file/' + image.Key,
-                    full_suffix: config.suffix.full
-                };
-                images.push(imageProps);
-            }
-            res.render('index', {
-                'images': images,
-                destinations: destinations
-            });
-        }
-    }); 
-    /*request.post(process.env.TRUSTIFI_URL + '/api/i/v1/email', {
-        headers: {
-            'x-trustifi-key': process.env.TRUSTIFI_KEY,
-            'x-trustifi-secret': process.env.TRUSTIFI_SECRET
-        },
-        json: {
-            "recipients": recipients,
-            "title": "New connection on Our Holidays",
-            "html": "We hope you enjoy the app !"
-        }
-    }, (err, res, body) => {
-        console.log(body);
-    });*/
-});
-
-app.post('/', async function (req, res, next) {
-    const {name, description} = req.body;
-    await Destination.create({name, description});
-    request.post(process.env.TRUSTIFI_URL + '/api/i/v1/email', {
-        headers: {
-            'x-trustifi-key': process.env.TRUSTIFI_KEY,
-            'x-trustifi-secret': process.env.TRUSTIFI_SECRET
-        },
-        json: {
-            "recipients": recipients,
-            "title": "Destination added !",
-            "html": "The Our Holidays app wishes you a good moment!"
-        }
-    }, (err, res, body) => {
-        console.log(body);
-    });
-    res.redirect('/');
-});
-
-app.get('/destinations', async function (req, res, next) {
-    let destinations = [];
-    try {
-        destinations = await Destination.findAll();
-    } catch (e) {
-    }
-    res.send(JSON.stringify(destinations, null, 2));
-});
-
-app.get('/file/:filename', function (req, res) {
-    const params = {
-        Bucket: config.s3.bucket,
-        Key: req.params.filename,
-    };
-
-    s3.getObject(params, function (err, data) {
-        if (err) {
-            res.send(err);
-        } else {
-            console.log(data);
-            res.contentType(data.ContentType);
-            res.send(data.Body);
-        }
-    });
-})
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
---
+++ routes.js
@@ -0,0 +1,206 @@
+const express = require('express');
+const config = require("./config");
+const router = express.Router();
+
+//Pour les formulaires multipart/form-data
+const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
+
+//Validation de formulaire
+const { body, validationResult } = require('express-validator');
+
+// database dependency
+const Destination = require("./destination");
+
+const AWS = require("aws-sdk");
+const Blitline = require("simple_blitline_node");
+const uuid = require("uuid");
+
+AWS.config.update(config.s3);
+const s3 = new AWS.S3();
+
+// const recipients = [
+//     {"email": "alois.zimmermann45@gmail.com"},
+//     {"email": "amedouillard@gmail.com"},
+//     {"email": "ichtitski@gmail.com"}
+// ]
+
+//Récupération des images
+router.get('/', async function (req, res) {
+    let destinations = [];
+    try {
+        destinations = await Destination.findAll();
+    } catch (e) {
+    }
+    const params = {
+        Bucket: config.s3.bucket,
+        Delimiter: '/'
+    };
+
+    const images = [];
+    s3.listObjects(params, function (err, data) {
+        if (err) {
+            res.send(err);
+        } else {
+            for (i in data.Contents) {
+                const image = data.Contents[i];
+                const imageProps = {
+                    url: '/file/' + image.Key,
+                    full_suffix: config.suffix.full
+                };
+                images.push(imageProps);
+            }
+            res.render('index', {
+                'images': images,
+                destinations: destinations
+            });
+        }
+    });
+    // request.post(process.env.TRUSTIFI_URL + '/api/i/v1/email', {
+    //     headers: {
+    //         'x-trustifi-key': process.env.TRUSTIFI_KEY,
+    //         'x-trustifi-secret': process.env.TRUSTIFI_SECRET
+    //     },
+    //     json: {
+    //         "recipients": recipients,
+    //         "title": "New connection on Our Holidays",
+    //         "html": "We hope you enjoy the app !"
+    //     }
+    // }, (err, res, body) => {
+    //     console.log(body);
+    // });
+});
+
+//Récupération des destinations stoquées en base
+router.get('/destinations', async function (req, res) {
+    let destinations = [];
+    try {
+        destinations = await Destination.findAll();
+    } catch (error) {
+        console.error(error);
+    }
+    res.send(JSON.stringify(destinations, null, 2));
+});
+
+//Récupération image
+router.get('/file/:filename', function (req, res) {
+    const params = {
+        Bucket: config.s3.bucket,
+        Key: req.params.filename,
+    };
+
+    s3.getObject(params, function (err, data) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.contentType(data.ContentType);
+            res.send(data.Body);
+        }
+    });
+})
+
+//Traitement du formulaire
+router.post('/',
+    upload.single('image'),
+    body('name')
+        .isLength({
+            min: 4
+        })
+        .withMessage("The destination name must be at least 4 characters").trim(),
+    body('description')
+        .isLength({
+            min: 8,
+            max: 255
+        }).withMessage("The description must be between 8-255 characters").trim().escape(),
+    async function (req, res) {
+
+        console.log("validation");
+
+        const errors = validationResult(req);
+        console.log(errors);
+        if(!errors.isEmpty()){
+            return res.render('./index.twig', {
+                data: req.body,
+                errors:  errors
+
+            });
+        }
+        const {name, description} = req.body;
+
+        //Upload l'image sur Wasabi
+        const bucket = config.s3.bucket;
+        if(req.file){
+
+            const params = {
+                Bucket: bucket,
+                Key: uuid.v4(),
+                Body: req.file.buffer
+            }
+
+            s3.upload(params, null,function (err, data) {
+                if(err){
+                    console.error('Erreur pendant l\'upload de l\'image')
+                    console.error(err)
+                }
+
+                //Retouche de l'image avec Blitline : ne fonctionne pas pour le moment
+                // console.log("KEY : " + data.key);
+                // const url = "https://s3.eu-west-1.wasabisys.com/our-holidays-test/" + data.key;
+                // const blitline = new Blitline()
+                // const blitlineAppId = config.blitline.BLITLINE_APPLICATION_ID;
+                // blitline.addJob({
+                //     "application_id": blitlineAppId,
+                //     "src" : url,
+                //     "functions" : [
+                //         {
+                //             "name": "resize_to_fit",
+                //             "params": {
+                //                 "width": "20",
+                //                 "height": "20"
+                //             },
+                //             "save" : {
+                //                 "image_identifier" : "MY_CLIENT_ID",
+                //                 "s3_destination": {
+                //                     "bucket": bucket,
+                //                     "key": "ALLALALALALALALAL"
+                //                 }
+                //             }
+                //         }]
+                // })
+                //
+                // let promise = blitline.postJobs();
+                // promise.then(function(data) {
+                //     console.log("data");
+                //     console.log(data.results);
+                //     console.log(data.results[0]);
+                // })
+
+            })
+        }
+
+        await Destination.create({name, description});
+
+        //Envoi de mail
+        // request.post(process.env.TRUSTIFI_URL + '/api/i/v1/email', {
+        //     headers: {
+        //         'x-trustifi-key': process.env.TRUSTIFI_KEY,
+        //         'x-trustifi-secret': process.env.TRUSTIFI_SECRET
+        //     },
+        //     json: {
+        //         "recipients": recipients,
+        //         "title": "Destination added !",
+        //         "html": "The Our Holidays app wishes you a good moment!"
+        //     }
+        // }, (err, res, body) => {
+        //     console.log(body);
+        // });
+
+        res.redirect('/');
+    });
+
+
+
+
+
+
+module.exports = router;
