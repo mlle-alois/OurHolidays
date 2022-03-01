6274b8b7e0498c31d6405a21419d05ab875a09e5
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2022-03-01 20:21:11 +0100

    Test erreur Sentry

--- routes.js
+++ routes.js
@@ -6,10 +6,10 @@ const tracing = require("@sentry/tracing")
 
 //Pour les formulaires multipart/form-data
 const multer = require('multer');
-const upload = multer({ storage: multer.memoryStorage() });
+const upload = multer({storage: multer.memoryStorage()});
 
 //Validation de formulaire
-const { body, validationResult } = require('express-validator');
+const {body, validationResult} = require('express-validator');
 
 // database dependency
 const Destination = require("./destination");
@@ -17,6 +17,8 @@ const Destination = require("./destination");
 const AWS = require("aws-sdk");
 const Blitline = require("simple_blitline_node");
 const uuid = require("uuid");
+AWS.config.update(config.s3);
+const s3 = new AWS.S3();
 
 sentry.init({
     dsn: "https://647642e8857d4c608d54b3e7e5f159b8:19b3a35905c84964a6bcb775f27affc3@o1156434.ingest.sentry.io/6237740",
@@ -33,14 +35,12 @@ sentry.init({
 //Récupération des images
 router.get('/', async function (req, res) {
     const transaction = sentry.startTransaction({
-        op: "get images",
+        op: "Get images",
         name: "Recuperation of images",
     });
-    
+
     setTimeout(async () => {
         try {
-            AWS.config.update(config.s3);
-            const s3 = new AWS.S3();
             let destinations = [];
             try {
                 destinations = await Destination.findAll();
@@ -93,30 +93,49 @@ router.get('/', async function (req, res) {
 
 //Récupération des destinations stoquées en base
 router.get('/destinations', async function (req, res) {
-    let destinations = [];
-    try {
-        destinations = await Destination.findAll();
-    } catch (error) {
-        console.error(error);
-    }
-    res.send(JSON.stringify(destinations, null, 2));
+    const transaction = sentry.startTransaction({
+        op: "Get destinations",
+        name: "Recuperation of destinations",
+    });
+    setTimeout(async () => {
+        try {
+            let destinations = [];
+            destinations = await Destination.findAll();
+            res.send(JSON.stringify(destinations, null, 2));
+        } catch (e) {
+            sentry.captureException(e);
+        } finally {
+            transaction.finish();
+        }
+    }, 99);
 });
 
 //Récupération image
 router.get('/file/:filename', function (req, res) {
+    const transaction = sentry.startTransaction({
+        op: "Get image",
+        name: "Recuperation of one image",
+    });
     const params = {
         Bucket: config.s3.bucket,
         Key: req.params.filename,
     };
-
-    s3.getObject(params, function (err, data) {
-        if (err) {
-            res.send(err);
-        } else {
-            res.contentType(data.ContentType);
-            res.send(data.Body);
+    setTimeout(async () => {
+        try {
+            s3.getObject(params, function (err, data) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.contentType(data.ContentType);
+                    res.send(data.Body);
+                }
+            });
+        } catch (e) {
+            sentry.captureException(e);
+        } finally {
+            transaction.finish();
         }
-    });
+    }, 99);
 })
 
 //Traitement du formulaire
@@ -133,94 +152,67 @@ router.post('/',
             max: 255
         }).withMessage("The description must be between 8-255 characters").trim().escape(),
     async function (req, res) {
+        const transaction = sentry.startTransaction({
+            op: "Treatment of form",
+            name: "Treatment of form",
+        });
+        setTimeout(async () => {
+            try {
+                const errors = validationResult(req);
+                console.log(errors);
+                if (!errors.isEmpty()) {
+                    return res.render('./index.twig', {
+                        data: req.body,
+                        errors: errors
 
-        console.log("validation");
+                    });
+                }
+                const {name, description} = req.body;
 
-        const errors = validationResult(req);
-        console.log(errors);
-        if(!errors.isEmpty()){
-            return res.render('./index.twig', {
-                data: req.body,
-                errors:  errors
+                //Upload l'image sur Wasabi
+                const bucket = config.s3.bucket;
+                if (req.file) {
 
-            });
-        }
-        const {name, description} = req.body;
+                    const params = {
+                        Bucket: bucket,
+                        Key: uuid.v4(),
+                        Body: req.file.buffer
+                    }
 
-        //Upload l'image sur Wasabi
-        const bucket = config.s3.bucket;
-        if(req.file){
-
-            const params = {
-                Bucket: bucket,
-                Key: uuid.v4(),
-                Body: req.file.buffer
-            }
-
-            s3.upload(params, null,function (err, data) {
-                if(err){
-                    console.error('Erreur pendant l\'upload de l\'image')
-                    console.error(err)
+                    s3.upload(params, null, function (err, data) {
+                        if (err) {
+                            console.error('Erreur pendant l\'upload de l\'image')
+                            console.error(err)
+                        }
+                    })
                 }
 
-                //Retouche de l'image avec Blitline : ne fonctionne pas pour le moment
-                // console.log("KEY : " + data.key);
-                // const url = "https://s3.eu-west-1.wasabisys.com/our-holidays-test/" + data.key;
-                // const blitline = new Blitline()
-                // const blitlineAppId = config.blitline.BLITLINE_APPLICATION_ID;
-                // blitline.addJob({
-                //     "application_id": blitlineAppId,
-                //     "src" : url,
-                //     "functions" : [
-                //         {
-                //             "name": "resize_to_fit",
-                //             "params": {
-                //                 "width": "20",
-                //                 "height": "20"
-                //             },
-                //             "save" : {
-                //                 "image_identifier" : "MY_CLIENT_ID",
-                //                 "s3_destination": {
-                //                     "bucket": bucket,
-                //                     "key": "ALLALALALALALALAL"
-                //                 }
-                //             }
-                //         }]
-                // })
-                //
-                // let promise = blitline.postJobs();
-                // promise.then(function(data) {
-                //     console.log("data");
-                //     console.log(data.results);
-                //     console.log(data.results[0]);
-                // })
+                await Destination.create({name, description});
 
-            })
-        }
+                //Envoi de mail
+                // request.post(process.env.TRUSTIFI_URL + '/api/i/v1/email', {
+                //     headers: {
+                //         'x-trustifi-key': process.env.TRUSTIFI_KEY,
+                //         'x-trustifi-secret': process.env.TRUSTIFI_SECRET
+                //     },
+                //     json: {
+                //         "recipients": recipients,
+                //         "title": "Destination added !",
+                //         "html": "The Our Holidays app wishes you a good moment!"
+                //     }
+                // }, (err, res, body) => {
+                //     console.log(body);
+                // });
 
-        await Destination.create({name, description});
+                res.redirect('/');
 
-        //Envoi de mail
-        // request.post(process.env.TRUSTIFI_URL + '/api/i/v1/email', {
-        //     headers: {
-        //         'x-trustifi-key': process.env.TRUSTIFI_KEY,
-        //         'x-trustifi-secret': process.env.TRUSTIFI_SECRET
-        //     },
-        //     json: {
-        //         "recipients": recipients,
-        //         "title": "Destination added !",
-        //         "html": "The Our Holidays app wishes you a good moment!"
-        //     }
-        // }, (err, res, body) => {
-        //     console.log(body);
-        // });
-
-        res.redirect('/');
+            } catch (e) {
+                sentry.captureException(e);
+            } finally {
+                transaction.finish();
+            }
+        }, 99);
     });
-
-
-
-
 
 
 module.exports = router;
